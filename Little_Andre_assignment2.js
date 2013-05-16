8a1a9f4ab6a99a09e56f96918c262a7e25151d14
Andre Little <ALittle@fullsail.edu>
2013-05-16 16:28:18 -0500

    Added Variables

--- Little_Andre_assignment2.js
+++ Little_Andre_assignment2.js
@@ -1,1 +1,17 @@
 // alert("JavaScript works!");
+
+// Andre Little
+// SDI 1305
+// Project 2
+// Hooby Upload
+
+
+// Variables
+var myName = "Andre Little";
+var myHobby = "YouTube";
+var content = "Video game commentaries";
+var process = ["Get game-play", "edit game-play", "commentate over gameplay", "make a thumbnail"];
+var minutesPerProcess = [120, 30, 15, 20];
+var nintendoConsoles = 4;
+var timeOfUpload = 60;
+var uploadComplete = true;
