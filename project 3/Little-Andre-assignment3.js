a574cbb991fb42194ddbe3c034aa4deab4041973
Andre Little <ALittle@fullsail.edu>
2013-05-24 16:58:40 -0500

    Finished JSON Data, starting Boolean

--- project 3/Little-Andre-assignment3.js
+++ project 3/Little-Andre-assignment3.js
@@ -1,1 +1,37 @@
-alert("JavaScript works!");
+// alert("JavaScript works!");
+
+// Andre Little
+// SDI 1305
+// Project 3
+// Video Game Tournament
+
+// Global Variables
+var teamName = "Zwo";
+    teamCaptain = "DaFightingRobot";
+    teamSlayer = "TheColinies";
+    teamSupport1 = "OhYeahCookies";
+    teamSupport2 = "MrHuntinNoobs";
+    consoleName = "Nintendo Wii U";
+    gameName = "Call of Duty: Black Ops 2";
+    mapCount = 5;
+    mapsWePlay = [" Raid", " HiJacked", " Plaza", " Carrier", " StandOff"];
+    minutesPerMap = [16, 10, 8, 8, 24];
+    playTime = true;
+    
+// JSON Data
+var myTeam = {
+    "zwoMembers": [
+        {"player": " DaFightingRobot",
+         "position": "Captain"
+        },
+        {"player": " TheColinies",
+         "position": "Slayer"
+        },
+        {"player": " OhYeahCookie",
+         "position": "Support"
+        },
+        {"player": " MrHuntinNoobs",
+         "position": "Support 2"
+        }
+    ]
+};
