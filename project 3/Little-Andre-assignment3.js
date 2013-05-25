826798b0cce7452ce861936729e917ab41c3fc0f
Andre Little <ALittle@fullsail.edu>
2013-05-24 22:10:32 -0500

    Finished the objects, and outputs

--- project 3/Little-Andre-assignment3.js
+++ project 3/Little-Andre-assignment3.js
@@ -99,6 +99,24 @@ var almostDone = {
     }
 };
 
+// Method, Accessor, and Mutator
+var timePlayedThisWeek = {
+    
+    "Monday": 4,
+    "Tuesday": 4,
+    "Thursday": 4,
+    "Friday": 8,
+    "Saturday": 2,
+    "timePlayed": function(){
+        var weekOneHours = this.Monday + this.Tuesday + this.Thursday + this.Friday + this.Saturday;
+        var hoursToMinutes = weekOneHours * 60;
+        return hoursToMinutes;
+    },
+    "morePractice": function(moreHours){
+        this.Saturday = moreHours
+    }
+};
+
 
 
 // Outputs
@@ -120,3 +138,13 @@ console.log ("Now, we have our strategies down for all most all game modes, we r
 console.log ("Lets recap the game modes and the time we spent on them.");
 console.log("We have practiced on" + almostDone.lastTouch(mapsWePlay, minutesPerMap) + ".");
 console.log ("Thanks to my team, team " + teamName + " and " + myTeam.zwoMembers[1].player + myTeam.zwoMembers[1].position + " for becoming MVP for the week." );
+console.log ("We practiced on Monday for " + timePlayedThisWeek.Monday + " hours.");
+console.log ("We practiced on Tuesday for " + timePlayedThisWeek.Tuesday + " hours.");
+console.log ("We practiced on Thursday for " + timePlayedThisWeek.Thursday + " hours.");
+console.log ("We practiced on Friday for " + timePlayedThisWeek.Friday + " hours.");
+console.log ("We practiced on Saturday for " + timePlayedThisWeek.Saturday + " hours.");
+console.log ("That is a total of " + timePlayedThisWeek.timePlayed() + " minutes.");
+console.log ("2 hours is not enough time for practice on Saturday, lets add 1 more hour.");
+timePlayedThisWeek.morePractice(3);
+console.log ("The new number of minutes we spent practicing is " + timePlayedThisWeek.timePlayed() + ".");
+console.log ("Now we are really ready for this Tournament.");
