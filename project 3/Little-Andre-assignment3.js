174cce97a1193b165e0a0c1b5c7b2af3796cd3cd
Andre Little <ALittle@fullsail.edu>
2013-05-24 21:26:30 -0500

    Finished the Array, working on the object

--- project 3/Little-Andre-assignment3.js
+++ project 3/Little-Andre-assignment3.js
@@ -14,24 +14,24 @@ var teamName = "Zwo";
     consoleName = "Nintendo Wii U";
     gameName = "Call of Duty: Black Ops 2";
     mapCount = 5;
-    mapsWePlay = [" Raid", " HiJacked", " Plaza", " Carrier", " StandOff"];
-    minutesPerMap = [16, 10, 8, 8, 24];
+    mapsWePlay = [" Raid", " HiJacked", " Plaza", " StandOff"];
+    minutesPerMap = [16, 10, 8, 24];
     playTime = true;
     
 // JSON Data
 var myTeam = {
     "zwoMembers": [
         {"player": " DaFightingRobot",
-         "position": "Captain"
+         "position": " the Captain"
         },
-        {"player": " TheColinies",
-         "position": "Slayer"
+        {"player": " Colinies",
+         "position": " the Slayer"
         },
         {"player": " OhYeahCookie",
-         "position": "Support"
+         "position": " the Support"
         },
         {"player": " MrHuntinNoobs",
-         "position": "Support 2"
+         "position": " the Wingman"
         }
     ]
 };
@@ -82,15 +82,22 @@ var ourChoice = demo.demoChoice('"A"', '"B"');
  
 // Array
 
-
-
-
-
-
-
-
-
-
+var almostDone = {
+    lastTouch: function(toDo, minutes){
+    for (var toDoNumber = 0; toDoNumber < toDo.length; toDoNumber++){
+        var actsDone = toDo;
+        var actName = toDo[toDoNumber];
+        var minutesPerAct = minutes[toDoNumber];
+        console.log("Started our match on the map " + actName + " for a total of " + minutesPerAct + " minutes.");
+        for (var min = 0; min < minutesPerAct; min+=2) {
+            var minPassed = minutesPerAct - min;
+            console.log(min + " minutes has passed by " + minPassed + " minutes to go.");
+        }
+            console.log("The match time has passed on" + actName + "."); 
+        }   return actsDone;
+    
+    }
+};
 
 
 
@@ -107,13 +114,9 @@ console.log ("After we got our game mode up in CTF, it's time to work on our Tea
 console.log ("The way TDM works is the first team to 7500pts wins, with each kill worth 100pts");
 console.log ("We have achieved " + teamDM.kills(0) + " pts. Now, on to the next mode.");
 console.log ("When we are confident enough to move on, we do the game mode Demolition on the map " + mapsWePlay[1] + ".");
-console.log ('Demolition is like SnD, but you have to get both the "A" and  "B" bombs planted in order to win.')
+console.log ('Demolition is like SnD, but you have to get both the "A" and  "B" bombs planted in order to win.');
 console.log (ourChoice);
-console.log ("")
-
-
-
-
-
-
-console.log ("Thanks to team " + teamName + " and their team " + myTeam.zwoMembers[0].position + myTeam.zwoMembers[0].player + " for a great game." );
+console.log ("Now, we have our strategies down for all most all game modes, we rest until tomorrow, where we will resume on the map  " + mapsWePlay[0] + ".")
+console.log ("Lets recap the game modes and the time we spent on them.");
+console.log("We have practiced on" + almostDone.lastTouch(mapsWePlay, minutesPerMap) + ".");
+console.log ("Thanks to my team, team " + teamName + " and " + myTeam.zwoMembers[1].player + myTeam.zwoMembers[1].position + " for becoming MVP for the week." );
